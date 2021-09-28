a141dd6e92caa9919b279cecb2447905ae3b23cd
Sarah D Herren <herren.sarah@icloud.com>
2021-09-28 15:04:23 -0500

    added get by id method

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -12,5 +12,51 @@ router.get('/', async (req, res) => {
         })
     }
 })
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const post = await Posts.findById(id)
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(post)
+        }
+    } catch(error) {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
+// router.verb('/', async (req, res) => {
+//     try {
+
+//     } catch(error) {
+
+//     }
+// })
+// router.verb('/', async (req, res) => {
+//     try {
+
+//     } catch(error) {
+
+//     }
+// })
+// router.verb('/', async (req, res) => {
+//     try {
+
+//     } catch(error) {
+
+//     }
+// })
+// router.verb('/', async (req, res) => {
+//     try {
+
+//     } catch(error) {
+
+//     }
+// })
+
 
 module.exports = router;

3d6ab40810781ba38aef42afd8e94bf71b5ac4b5
Sarah D Herren <herren.sarah@icloud.com>
2021-09-28 14:59:49 -0500

    added get all posts method

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,4 +2,15 @@ const express = require('express');
 const Posts = require('./posts-model');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        })
+    }
+})
+
 module.exports = router;

b9abfae24103c85d63ed68356cf2c0ab13f0a5b1
Sarah D Herren <herren.sarah@icloud.com>
2021-09-28 20:16:50 -0500

    added find comments method

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -96,13 +96,25 @@ router.delete('/:id', async (req, res) => {
         })
     }
 })
-// router.verb('/', async (req, res) => {
-//     try {
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const { id } = req.params
+        const post = await Posts.findById(id)
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            const comments = await Posts.findPostComments(id)
+            res.status(200).json(comments)
+        }
 
-//     } catch(error) {
-
-//     }
-// })
+    } catch(error) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved"
+        })
+    }
+})
 
 
 module.exports = router;
